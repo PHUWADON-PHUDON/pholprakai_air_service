4fcc394577583d4f4739703d5a6cb7fcd4f6f12f
phuwadon <ppuwdon@gmail.com>
2026-07-29 19:29:53 +0700

    feat: enhance responsiveness and add mobile menu functionality

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -10,20 +10,34 @@ import TikTok from "../icons/TikTok";
 export default function Footer() {
     return(
         <footer className="rounded-t-[50px] overflow-hidden bg-blue-2">
-            <div className="p-[30px_30px] grid grid-cols-[1fr_1fr_1fr] gap-[30px]">
-                <div>
-                    <div className="flex items-center gap-1 justify-self-start">
+            <div className={`
+                p-[30px_30px] grid grid-cols-[1fr_1fr_1fr] gap-[30px]
+                max-[965px]:grid-cols-[1fr_1fr]
+                max-[655px]:grid-cols-[1fr]
+            `}>
+                <div className={`
+                    max-[655px]:flex max-[655px]:flex-col max-[655px]:items-center
+                `}>
+                    <div className={`
+                        flex items-center gap-1 justify-self-start
+                    `}>
                         <Snowflake color="white"/>
                         <h1 className="text-[25px] font-bold text-white">พลประกาย แอร์ เซอร์วิส</h1>
                     </div>
-                    <div className="mt-[10px]">
+                    <div className={`
+                        mt-[10px]
+                        max-[655px]:flex max-[655px]:justify-center max-[655px]:flex-wrap
+                    `}>
                         <span className="mr-[10px] mt-[10px] text-[16px] text-white inline-block">ติดตั้งแอร์ใหม่</span>
                         <span className="mr-[10px] mt-[10px] text-[16px] text-white inline-block">ย้ายแอร์</span>
                         <span className="mr-[10px] mt-[10px] text-[16px] text-white inline-block">เช็ครั่ว เติมน้ำยา</span>
                         <span className="mr-[10px] mt-[10px] text-[16px] text-white inline-block">ตรวจเช็ค</span>
                         <span className="mr-[10px] mt-[10px] text-[16px] text-white inline-block">วิเคราะห์อาการเสีย</span>
                     </div>
-                    <div className="mt-[20px] flex flex-wrap gap-1">
+                    <div className={`
+                        mt-[20px] flex flex-wrap gap-1
+                        max-[655px]:flex max-[655px]:justify-center max-[655px]:flex-wrap
+                    `}>
                         <MapPin color="white"/>
                         <span className="text-white">ชลบุรี: </span>
                         <span className="text-white">เมืองชลบุรี</span>
@@ -37,7 +51,9 @@ export default function Footer() {
                 </div>
                 <div className="flex flex-col items-center">
                     <p className="text-[20px] font-bold text-white mt-[10px]">ติดต่อ</p>
-                    <div>
+                    <div className={`
+                        max-[655px]:flex max-[655px]:justify-center max-[655px]:flex-wrap max-[655px]:gap-2
+                    `}>
                         <div className="flex items-center mt-[10px] gap-2">
                             <Phone color="white"/>
                             <p className="text-white">โทร: 098-267-1789</p>
@@ -48,11 +64,21 @@ export default function Footer() {
                         </div>
                         <div className="flex items-center mt-[10px] gap-2">
                             <Mail color="white"/>
-                            <p className="text-white">อีเมล: tumairservice.chon@gmail.com</p>
+                            <p className={`
+                                text-white
+                                max-[655px]:hidden
+                            `}>อีเมล: tumairservice.chon@gmail.com</p>
+                            <p className={`
+                                hidden text-white
+                                max-[655px]:block
+                            `}>tumairservice.chon@gmail.com</p>
                         </div>
                     </div>
                 </div>
-                <div>
+                <div className={`
+                    max-[965px]:col-span-2
+                    max-[655px]:col-span-1
+                `}>
                     <div className="flex flex-col items-center">
                         <p className="text-[20px] font-bold text-white mt-[10px]">ติดตาม</p>
                         <div className="w-full">
--- src/components/common/MainMenu.tsx
+++ src/components/common/MainMenu.tsx
@@ -1,15 +1,119 @@
+"use client";
+import { useEffect, useRef } from "react";
 import { MenuList } from "@/lib/utils/MenuList";
+import Menu from "../icons/Menu";
 
 export default function MainMenu() {
+    const bgMobileMenu = useRef<HTMLDivElement | null>(null);
+    const navMobileMenu = useRef<HTMLDivElement | null>(null);
+    const timeoutRefs = useRef<(NodeJS.Timeout | undefined)[]>([]);
+    const isToggle = useRef(false);
+
+    const clickMenu = () => {
+        let time1: NodeJS.Timeout | undefined
+        let time2: NodeJS.Timeout | undefined
+        let time3: NodeJS.Timeout | undefined
+
+        timeoutRefs.current.forEach((t) => t && clearTimeout(t));
+        timeoutRefs.current = [];
+
+        if (!isToggle.current) {
+            time1 = setTimeout(() => {
+                if (!bgMobileMenu.current) return;
+                bgMobileMenu.current.style.display = "block";
+            },10);
+    
+            time2 = setTimeout(() => {
+                if (!bgMobileMenu.current) return;
+                bgMobileMenu.current.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+            },20);
+    
+            time3 = setTimeout(() => {
+                if (!navMobileMenu.current) return;
+                navMobileMenu.current.style.transform = "translateX(200px)";
+            },30);
+
+            isToggle.current = true;
+        }
+        else if (isToggle.current) {
+            time1 = setTimeout(() => {
+                if (!bgMobileMenu.current) return;
+                if (!navMobileMenu.current) return;
+                navMobileMenu.current.style.transform = "translateX(-200px)";
+                bgMobileMenu.current.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+            },10);
+
+            time2 = setTimeout(() => {
+                if (!bgMobileMenu.current) return;
+                bgMobileMenu.current.style.display = "none";
+            },200);
+
+            isToggle.current = false;
+        }
+
+        timeoutRefs.current = [time1, time2, time3];
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 730) {
+                if (!bgMobileMenu.current) return;
+                if (!navMobileMenu.current) return;
+
+                timeoutRefs.current.forEach((t) => t && clearTimeout(t));
+                timeoutRefs.current = [];
+
+                navMobileMenu.current.style.transform = "translateX(-200px)";
+                bgMobileMenu.current.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+                bgMobileMenu.current.style.display = "none";
+
+                isToggle.current = false;
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            timeoutRefs.current.forEach((t) => clearTimeout(t));
+        };
+    }, []);
+
     return(
-        <div>
-            <div className="flex gap-3.5">
-                {MenuList.map((item, i) => (
-                    <a key={i} href={item.path} className="font-bold">
-                        {item.name}
-                    </a>
-                ))}
+        <>
+            <div>
+                <div className={`
+                    flex gap-3.5
+                    max-[730px]:hidden    
+                `}>
+                    {MenuList.map((item, i) => (
+                        <a key={i} href={item.path} className="font-bold">
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div className={`
+                    hidden
+                    max-[730]:block
+                `}>
+                    <div onClick={() => clickMenu()} className="cursor-pointer">
+                        <Menu/>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div onClick={() => clickMenu()} ref={bgMobileMenu} className={`
+                hidden fixed top-0 left-0 w-full h-full overflow-hidden duration-200
+            `}>
+                <div ref={navMobileMenu} onClick={(e) => e.stopPropagation()} className={`
+                    w-[200px] translate-x-[-200px] bg-blue-2 h-full p-[20px] flex flex-col gap-[20px] duration-200
+                `}>
+                    {MenuList.map((item, i) => (
+                        <a key={i} href={item.path} className="font-bold text-white">
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
